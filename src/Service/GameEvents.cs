c937fd9796d119bc9fcde9f6f5f9ed427c4df1d9
Nicholas Jordaan <njordaan99@gmail.com>
2024-02-17 13:42:03 +0200

    chore: split events into separate files for ease of dev

--- src/Service/GameEvents.cs
+++ src/Service/GameEvents.cs
@@ -1,94 +1,18 @@
-﻿using GameNetcodeStuff;
+﻿namespace QualityCompany.Service;
 
-namespace QualityCompany.Service;
-
-public class GameEvents
+public partial class GameEvents
 {
-    private static readonly ACLogger _logger = new(nameof(GameEvents));
-
-    public static event HUDEvent HudManagerStart;
-    public delegate void HUDEvent(HUDManager instance);
+    private static readonly ACLogger Logger = new(nameof(GameEvents));
 
     public static event TimeUpdateEvent GameTimeUpdate;
     public delegate void TimeUpdateEvent();
 
-    public delegate void PlayerControllerEvent(PlayerControllerB instance);
-    public delegate void PlayerControllerRpcEvent(PlayerControllerB instance, bool isLocalPlayerInstance);
-    // Player rpc actions
-    public static event PlayerControllerRpcEvent PlayerGrabObjectClientRpc;
-    public static event PlayerControllerRpcEvent PlayerSwitchToItemSlot; // seems this is actually is a Rpc call in the background
-    public static event PlayerControllerRpcEvent PlayerThrowObjectClientRpc;
-    public static event PlayerControllerRpcEvent PlayerDropAllHeldItems; // seems this is actually is a Rpc call in the background
-    // Player non-rpc actions (triggers on local client only) - well hopefully
-    public static event PlayerControllerEvent PlayerDiscardHeldObject;
-
-    // Player life? actions
-    public static event PlayerControllerEvent PlayerDeath;
-
-    // Player item actions
-    public static event PlayerControllerEvent PlayerShotgunShoot;
-    public static event PlayerControllerEvent PlayerShotgunReload;
-
     public static event DisconnectEvent Disconnected;
     public delegate void DisconnectEvent(GameNetworkManager instance);
 
-    internal static void OnHudManagerStart(HUDManager instance)
-    {
-        _logger.LogDebug("OnHudManagerStart");
-        HudManagerStart?.Invoke(instance);
-    }
-
-    internal static void OnPlayerGrabObjectClientRpc(PlayerControllerB instance, GrabbableObject go)
-    {
-        _logger.LogDebug("OnPlayerGrabObjectClientRpc");
-        PlayerGrabObjectClientRpc?.Invoke(instance, instance == GameNetworkManager.Instance.localPlayerController);
-    }
-
-    internal static void OnPlayerSwitchToItemSlot(PlayerControllerB instance)
-    {
-        _logger.LogDebug("OnPlayerSwitchToItemSlot");
-        PlayerSwitchToItemSlot?.Invoke(instance, instance == GameNetworkManager.Instance.localPlayerController);
-    }
-
-    internal static void OnPlayerThrowObjectClientRpc(PlayerControllerB instance)
-    {
-        _logger.LogDebug("OnPlayerDiscardHeldObject");
-        PlayerThrowObjectClientRpc?.Invoke(instance, instance == GameNetworkManager.Instance.localPlayerController);
-    }
-
-    internal static void OnPlayerDropAllHeldItems(PlayerControllerB instance)
-    {
-        _logger.LogDebug("OnPlayerDropAllHeldItems");
-        PlayerDropAllHeldItems?.Invoke(instance, instance == GameNetworkManager.Instance.localPlayerController);
-    }
-
-    internal static void OnPlayerDiscardHeldObject(PlayerControllerB instance)
-    {
-        _logger.LogDebug("OnPlayerDiscardHeldObject");
-        PlayerDiscardHeldObject?.Invoke(instance);
-    }
-
-    internal static void OnPlayerShotgunShoot(PlayerControllerB instance)
-    {
-        _logger.LogDebug("OnPlayerShootShotgun");
-        PlayerShotgunShoot?.Invoke(instance);
-    }
-
-    internal static void OnPlayerDeath(PlayerControllerB instance)
-    {
-        _logger.LogDebug($"OnPlayerDeath -> {instance.playerUsername}");
-        PlayerDeath?.Invoke(instance);
-    }
-
-    internal static void OnPlayerShotgunReload(PlayerControllerB instance)
-    {
-        _logger.LogDebug("OnPlayerShotgunReload");
-        PlayerShotgunReload?.Invoke(instance);
-    }
-
     internal static void OnDisconnected(GameNetworkManager instance)
     {
-        _logger.LogDebug("OnDisconnected");
+        Logger.LogDebug("OnDisconnected");
         Disconnected?.Invoke(instance);
 
         // HUD
@@ -105,6 +29,9 @@ public class GameEvents
 
         PlayerShotgunShoot = null;
         PlayerShotgunReload = null;
+
+        // Terminal
+        TerminalAwakeEvent = null;
 
         // Game
         Disconnected = null;
---
+++ src/Service/TerminalEvents.cs
@@ -0,0 +1,21 @@
+﻿namespace QualityCompany.Service;
+
+/// <summary>
+/// Terminal specific game events
+/// </summary>
+public partial class GameEvents
+{
+    public delegate void TerminalEvent(Terminal instance);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static event TerminalEvent TerminalAwakeEvent;
+    // public static event TerminalEvent TerminalAwakeEvent;
+
+    internal static void OnTerminalAwakeEvent(Terminal instance)
+    {
+        Logger.LogDebug("TerminalAwakeEvent");
+        TerminalAwakeEvent?.Invoke(instance);
+    }
+}
